4039d932b397d2a3ccbf1d31c3e0f5858fe1e780
gabricosttt <gabrielecostanza161@gmail.com>
2026-04-09 11:48:19 +0200

    progetto completo

--- esercizioQuattro.py
+++
@@ -1,9 +0,0 @@
-X = df[['umidita', 'temp', 'ore_secco']]
-y = df['irrigare']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-modello = RandomForestClassifier(n_estimators=50, random_state=42)
-modello.fit(X_train, y_train)
-y_pred = modello.predict(X_test)
-
-print(f"{accuracy_score(y_test, y_pred)*100:.1f}%")
